e7851aa097874fd228cd506a2c3bd162f26dc350
gucong <gucong@gmail.com>
2013-12-11 16:41:51 +0800

    修正若干IE下问题

--- src/jquery.h5f.js
+++ src/jquery.h5f.js
@@ -10,15 +10,13 @@
 (function($){
 	var input = $("<input>")[0],
 		support = "validity" in input && "checkValidity" in input,
+		doc = document,
 		initInvalid;	
 
 	//如果浏览器不支持HTML5,则加载h5f.js
-	(function(d){
-		if(!support){
-			var scrNode = $(d.scripts ? d.scripts[d.scripts.length - 1] : "script:last");
-			scrNode.attr("src", scrNode.attr("data-h5f"));
-		}
-	})(document);
+	if(!support){
+		doc.writeln("<script src=\""+ $(doc.scripts ? doc.scripts[doc.scripts.length - 1] : "script:last").attr("data-h5f")+"\"></script>");
+	}
 
 	//回调的触发
 	function validityCall(opt, e){
@@ -32,7 +30,7 @@
 		var v = node.validity;
 		if(v) {
 			node = $(node);
-	
+
 			opt.validClass && node.toggleClass(opt.validClass, v.valid);
 			opt.invalidClass && node.toggleClass(opt.invalidClass, (!v.valueMissing && !v.valid));
 			opt.requiredClass && node.toggleClass(opt.requiredClass, v.valueMissing);
@@ -40,37 +38,58 @@
 		}
 	}
 
-	//支持HTML5验证的浏览器，为了去掉默认样式，阻止全部浏览器默认行为；
-	initInvalid = support ? function(form, opt){
-		function prevent(node){
-			$(node).bind("invalid", function(e){
-				e.preventDefault();
-				validityCall(opt, e);
-			})
+	function initInvalid(form, opt){
+
+		//invalid事件处理函数
+		function invalidFn(e){
+			//阻止事件冒泡
+			e.stopPropagation();
+			//为了去掉默认样式，阻止全部浏览器默认行为；
+			e.preventDefault();
+			validityCall(opt, e);
 		}
+
+		//绑定事件处理函数
+		function prevent(node){
+			return $(node).bind("invalid", invalidFn);
+		}
+
+		//不支持HTML5验证的浏览器，使用H5F
+		if(!support){
+			H5F.setup(form, opt);
+		}
+
 		//将现有表单元素去除默认行为
 		prevent(form.elements);
-		$(form).bind("DOMNodeInserted", function(e){
-			var target = e.target;
-			if("validity" in target && "checkValidity" in target){
-				//将动态添加的元素去除默认行为
-				prevent(target);
+		form = prevent(form).delegate(":text,textarea", "change", function(e){
+			//文本框自动trim
+			var input = e.target;
+
+			if(/^\s+$/.test(input.value)){
+				input.value = $.trim(input.value);
 			}
-		}).delegate(":submit", "click", function(e){
-			//由于阻止了默认事件，需要重新模拟焦点行为
-			var invalid = this.form.querySelector(":invalid");
-			invalid && invalid.focus();
-		}).bind("change input", function(e){
-			toggleClass(e.targe, opt);
 		});
-	} : function(form, opt){
-		//不支持HTML5验证的浏览器，使用H5F
-		opt.jQuery = $;
-		H5F.setup(form, opt);
-		$(form).bind("invalid", function(e){
-			e.stopImmediatePropagation();
-			validityCall(opt, e);
-		});
+
+		if(support || doc.createEvent) {
+
+			form.bind("DOMNodeInserted", function(e){
+				var target = e.target;
+				if("validity" in target && "checkValidity" in target){
+					//将动态添加的元素去除默认行为
+					prevent(target);
+				}
+			});
+
+			if(support){
+				//由于阻止了默认事件，需要重新模拟焦点行为
+				form.delegate(":submit", "click", function(e){
+					var invalid = this.form.querySelector(":invalid");
+					invalid && invalid.focus();
+				}).bind("change", function(e){
+					toggleClass(e.target, opt);
+				});
+			}
+		}
 	};
 
 	/*表单验证公共组件*/
@@ -85,9 +104,13 @@
 		if(opt.events){
 			for(var i in opt.events){
 				this.delegate(i, opt.events[i], function(e){
-					if(e.target.checkValidity && e.target.checkValidity()){
-						validityCall(opt, e);
-					}
+					var input = e.target;
+					//狗日的IE10、11在change事件发生时select标签的validity还未更新，所以延迟
+					window.setTimeout(function(){
+						if(input.checkValidity && input.checkValidity()){
+							validityCall(opt, e);
+						}
+					}, 1);
 				});
 			}
 		}
@@ -95,6 +118,6 @@
 		return this.each(function(){
 			initInvalid(this, opt);
 		});
-	}
+	};
 
 })(jQuery);
